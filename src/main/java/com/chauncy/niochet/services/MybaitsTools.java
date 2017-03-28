1c24987ba333a52c003c3c3b962b4c46f0762a13
chauncy <1396912801@qq.com>
2017-03-28 17:00:44 +0800

    window

--- src/main/java/com/chauncy/niochet/services/MybaitsTools.java
+++ src/main/java/com/chauncy/niochet/services/MybaitsTools.java
@@ -1,8 +1,16 @@
 package com.chauncy.niochet.services;
 
+import org.apache.ibatis.session.SqlSession;
+
 /**
  * Mybaits 的服务层实现
  * Created by chauncy on 17-3-28.
  */
 public class MybaitsTools {
+	private SqlSession sqlSession;
+
+	public MybaitsTools() {
+
+
+	}
 }

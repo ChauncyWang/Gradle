e995f54a1375982c8dee4baf9353cc9b408a3afb
Chauncy <1396912801@qq.com>
2017-03-30 00:43:18 +0800

    成功移植window环境

--- src/main/java/com/chauncy/niochet/server/ServerMain.java
+++ src/main/java/com/chauncy/niochet/server/ServerMain.java
@@ -1,11 +1,14 @@
 package com.chauncy.niochet.server;
 
+import com.chauncy.niochet.server.services.MybaitsTools;
+
 /**
  * 服务器主线程
  * Created by chauncy on 17-3-18.
  */
 public class ServerMain {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ClassNotFoundException {
+        MybaitsTools.getSqlSession();
 		NIOSocketThread nioSocketThread = new NIOSocketThread(10001);
 
 		new Thread(nioSocketThread).start();

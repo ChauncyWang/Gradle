dc71b7d0e2046e2afcefc86e9608f41229d0f58c
Chauncy <1396912801@qq.com>
2017-03-30 10:33:59 +0800

    成功移植window环境

--- src/main/java/com/chauncy/niochet/entity/Friend.java
+++ src/main/java/com/chauncy/niochet/entity/Friend.java
@@ -1,34 +1,67 @@
 package com.chauncy.niochet.entity;
 
+import java.util.List;
+
 /**
  * 朋友关系类
  * Created by chauncy on 17-3-29.
  */
 public class Friend {
-	private String uId;
-	private String fId;
+	/**
+	 * 分组
+	 */
+	private List<FriendGroup> groups;
 
 	public Friend() {
 	}
 
-	public Friend(String uId, String fId) {
-		this.uId = uId;
-		this.fId = fId;
+	public Friend( List<FriendGroup> groups) {
+		this.groups = groups;
 	}
 
-	public String getuId() {
-		return uId;
+	public List<FriendGroup> getGroups() {
+		return groups;
 	}
 
-	public void setuId(String uId) {
-		this.uId = uId;
+	public void setGroups(List<FriendGroup> groups) {
+		this.groups = groups;
+	}
+}
+
+/**
+ * 分组类
+ */
+class FriendGroup {
+	/**
+	 * 分组名
+	 */
+	private String name;
+	/**
+	 * 分组用户信息
+	 */
+	private List<UserInfo> friend;
+
+	public FriendGroup() {
 	}
 
-	public String getfId() {
-		return fId;
+	public FriendGroup(String name, List<UserInfo> friend) {
+		this.name = name;
+		this.friend = friend;
 	}
 
-	public void setfId(String fId) {
-		this.fId = fId;
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<UserInfo> getFriend() {
+		return friend;
+	}
+
+	public void setFriend(List<UserInfo> friend) {
+		this.friend = friend;
 	}
 }

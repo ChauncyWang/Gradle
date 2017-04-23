9ada9d696e18ff9b2d6bb60d7870798a81f616b6
chauncyw <13969122801@qq.com>
2017-04-24 00:08:32 +0800

    FoC

---
+++ FoC/src/main/java/cc/chauncy/foc/Main.java
@@ -0,0 +1,10 @@
+package cc.chauncy.foc;
+
+/**
+ * Created by chauncyw on 17-4-24.
+ */
+public class Main {
+    public static void main(String[] args){
+        System.out.println("GGGG");
+    }
+}
